c3536b3503c8973c78bab522c4c23327c4f98af6
ali azimzadeh <aliazimzadeh@gmail.com>
2022-02-08 08:41:11 +0330

    first commit

---
+++ OOP/Inheritence/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp_OOP.Inheritence
+{
+    class Vehicle : object // base class (parent) 
+    {
+        public Vehicle() : base()
+        {
+        }
+
+        public string brand = "Ford";  // Vehicle field
+        public void honk()             // Vehicle method 
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+}
